5e589398319a668fe58e0bcfbb926ee5ccd8207e
Amanda Medrado <amandacmedrado71@gmail.com>
2026-03-03 21:32:35 -0300

    Subindo ajustes

--- src/main/java/com/experian/payment/api/ApiApplication.java
+++ src/main/java/com/experian/payment/api/ApiApplication.java
@@ -1,4 +1,4 @@
-package com.experian.payment.api.domain;
+package com.experian.payment.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/experian/payment/api/controller/ProductController.java
+++ src/main/java/com/experian/payment/api/controller/ProductController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping({"products"})
+@RequestMapping({"/products"})
 public class ProductController {
     @Autowired
     private ProductRepository repository;
